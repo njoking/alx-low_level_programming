d6eeac5a90ae009aa4892825f5c0874b20319839
njoking <njokingugi9462@gmail.com>
2023-08-10 17:42:13 -0700

    yeeey

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-
 /**
- *main-This is the main function
- *Return:always0
+ * main- This is the main funtion
+ * Return: always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle.");
+	return (0);
+
 }
