48c8d0b86e83831bec367458b734f4be2f69ffa1
njoking <njokingugi9462@gmail.com>
2023-08-21 04:57:04 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the integer in question to 98
+ * @n: this is the integer in question
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
