dd04672e893310c60a3889d35b5bbe401dfe2bf1
njoking <njokingugi9462@gmail.com>
2023-08-28 12:05:16 -0700

    mm

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
- /**
- *set_string -  sets the value of a pointer to a char.
- *@s: character
- *@to:character
- *Return: void
+/**
+ * set_string -  sets the value of a pointer to a char.
+ * @s: a character
+ * @to: another character
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,43 @@
 #include "main.h"
 /**
- * _strstr - locates a substring.
- * @haystack: thisis the string in question
- * @needle: this is the substring in question
- * Return: pointer to the beginning of the located string or NULL
+ * _strstr - function that locates a substring.
+ *@haystack: first value -char
+ *@needle: second value - char
+ *
+ * Return: char with result
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
-		{
-			return (haystack);
-		}
-		else
-		{
-			haystack++;
-		}
-	}
-	return (0);
-}
+	int count = 0, a = 0, k, j = 0, l, i = 0;
+	char *p;
 
+	while (needle[count] != '\0')
+	{
+		count++;
+	}
+	while (haystack[i] != '\0')
+	{
+		i++;
+			}
+	if (count == 0)
+		return (haystack);
+	while (haystack[j] != '\0')
+	{
+		if (haystack[j] == needle[0])
+		{
+			p = &haystack[j];
+			l = j;
+			a = 0;
+			for (k = 0; k < count; k++)
+			{
+				if (haystack[l] == needle[k])
+					a++;
+				l++;
+			}
+		}
+		if (a == count)
+			return (p);
+		j++;
+	}
+	return ('\0');
+}
