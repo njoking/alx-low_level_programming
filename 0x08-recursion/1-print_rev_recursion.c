3b678809f992e9e339504519d0cf75f32e905ce3
njoking <njokingugi9462@gmail.com>
2023-08-30 11:58:12 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -  that prints a string in reverse.
+ * @s: this is the character in question
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
