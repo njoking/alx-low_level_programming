516cd71690f404f64602ed688d5fa8049ae2dcd6
njoking <njokingugi9462@gmail.com>
2023-08-30 13:37:53 -0700

    mm

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,30 @@
-#include "main.h"
 /**
- *  _sqrt_recursion -  returns the natural square root
- *  @n: character in question
- *  Return: square root
+ * check_sqrt - get if a number is sqrt of another
+ *
+ * @x: number to reach
+ * @y: number to check
+ *
+ * Return: sqrt of x, -1 otherwise
  */
-int _sqrt_recursion(int n)
+int check_sqrt(int x, int y)
 {
-	if (n * n == n)
-		return (b);
-	else if (n * n > 0)
+	if (y * y > x)
 		return (-1);
-	return (_sqrt_recursion(n, n + 1));
+	if (y * y == x)
+		return (y);
+	return (check_sqrt(x, y + 1));
+}
+
+/**
+ * _sqrt_recursion - gets natural square root of a number, if any
+ *
+ * @x: number to get square root of
+ *
+ * Return: natural square root of number, -1 otherwise
+ */
+int _sqrt_recursion(int x)
+{
+	if (x < 0)
+		return (-1);
+	return (check_sqrt(x, 0));
 }
