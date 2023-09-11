ace1056375a7526e79fd189d7f0ba4bed809767c
njoking <njokingugi9462@gmail.com>
2023-09-11 10:48:50 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - creates a dog struct
+ * @name: name of the dog
+ * @age: the dog age
+ * @owner: the owner of the dog
+ *
+ * Desc: first struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
