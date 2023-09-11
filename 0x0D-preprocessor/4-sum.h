7a673baf1ed45de0e7f12630bcdb7ca0be5a2152
njoking <njokingugi9462@gmail.com>
2023-09-11 10:15:02 -0700

    task 5

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,10 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * file - 4-sum
+ * @x: integer in question
+ * @y: integer in question
+ * Desc: sum of x and y
+ */
+#define SUM(x, y) (x + y)
+#endif
