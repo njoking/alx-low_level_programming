b13a58c64cebf6cb10099a53d277bee64994832e
njoking <njokingugi9462@gmail.com>
2023-09-11 14:08:41 -0700

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,27 @@
-#ifndef DOG_H_
-#define DOG_H_
+#ifndef DOGH
+#define DOGH
+
 /**
- * struct dog - creates a dog struct
- * @name: name of the dog
- * @age: the dog age
- * @owner: the owner of the dog
+ * struct dog - a dog
  *
- * Desc: first struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
+} dog;
 
-}dog_t;
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+int _putchar(char c);
+
 #endif
