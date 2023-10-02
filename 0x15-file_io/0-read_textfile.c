dbd9a25c3add69ff4fcfeed5577558504b0421c8
njoking <njokingugi9462@gmail.com>
2023-10-02 10:48:04 -0700

    taask 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a textfile and prints
+ * @filename: the file in question
+ * @letters : character in question
+ * Return: 0 or expected number of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int length, wrotechars;
+	char *buf;
+
+	if (filename == NULL || letters == 0)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
+	free(buf);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
+}
